e7074631aa41d74b47e7b790b495ceeece48387e
Georg Krispel <georg.krispel@icg.tugraz.at>
2022-06-21 22:46:31 +0200

    vscode extensions

--- scripts/programs/vscode.sh
+++ scripts/programs/vscode.sh
@@ -4,3 +4,10 @@ echo "🐋 Installing VSCode"
 wget -O vscode.deb 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64'
 sudo apt install ./vscode.deb
 rm -rf ./vscode.deb
+
+code --install-extension ms-python.python
+code --install-extension ms-vscode.cpptools
+code --install-extension james-yu.latex-workshop
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension 076923.python-image-preview
+code --install-extension eamodio.gitlens
