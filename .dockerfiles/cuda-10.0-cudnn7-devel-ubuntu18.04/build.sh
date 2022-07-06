aae0260c7ae7e4f754713b57f929e384b1926784
Georg Krispel <georg.krispel@icg.tugraz.at>
2022-07-06 11:23:11 +0200

    added dockerfiles

---
+++ .dockerfiles/cuda-10.0-cudnn7-devel-ubuntu18.04/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+TAG="gfkri/cuda:ubuntu-18.04_cuda-10.0_cudnn-7"
+docker build -t $TAG .
+DANGLING_IMAGES=$(docker images -f "dangling=true" -q)
+if [ ! -z "$DANGLING_IMAGES" ]
+then
+    docker rmi $DANGLING_IMAGES
+fi
