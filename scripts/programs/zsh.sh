0591986655cea222cd1f744b845a2d482763b31c
Georg Krispel <georg.krispel@icg.tugraz.at>
2022-07-18 13:19:38 +0200

    updated zsh installation

--- scripts/programs/zsh.sh
+++ scripts/programs/zsh.sh
@@ -5,6 +5,8 @@ source ./utils.sh
 
 apt_install zsh
 chsh -s /usr/bin/zsh
+export RUNZSH=no
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cd ../data
+cp .zshrc ${HOME}
 cp -r .oh-my-zsh ${HOME}
