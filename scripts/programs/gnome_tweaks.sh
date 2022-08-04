846827471ee08bb1162f37118a10e03683909d28
Georg Krispel <georg.krispel@icg.tugraz.at>
2022-08-04 10:23:55 +0200

    added system monitor ing gnome extensions

--- scripts/programs/gnome_tweaks.sh
+++ scripts/programs/gnome_tweaks.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 source ./utils.sh
-echo "📦 Installing Gnome Tweaks"
+echo "📦 Installing Gnome Tweaks (Require Gnome Restart)"
+
+install_extension () {
+    gnome-extensions enable "$1"
+}
 
 sudo add-apt-repository -y universe
 apt_install gnome-tweaks
 apt_install gnome-shell-extensions
 apt_install gnome-shell-extension-manager
 
+# Dependencies Gnome System Monitor
+apt_install gir1.2-gtop-2.0 
+apt_install gir1.2-nm-1.0 
+apt_install gir1.2-clutter-1.0 
+apt_install gnome-system-monitor
+
+install_extension system-monitor-next@paradoxxx.zero.gmail.com
