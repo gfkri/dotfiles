61c087dec91b14e21a26de46bbca0be4e1e308f4
Georg Krispel <georg.krispel@icg.tugraz.at>
2023-08-28 19:03:08 +0200

    improved postscript

--- data/zotero_bb_postscript.js
+++ data/zotero_bb_postscript.js
@@ -1,30 +1,39 @@
 if (Translator.BetterBibTeX) {
-    tex.remove('isbn');
-    tex.remove('publisher');
+  tex.remove('isbn');
+  tex.remove('publisher');
+  tex.remove('address');
+  tex.remove('doi');
+  tex.remove('month');
+  tex.remove('editor');
+  tex.remove('series');
+  tex.remove('url');
+  tex.remove('urldate');
+
+  if (tex.entrytype === 'inproceedings') {
+    tex.remove('pages');
+    tex.remove('volume');
     tex.remove('address');
-    tex.remove('doi');
-    tex.remove('month');
-    tex.remove('editor');
-    tex.remove('series');
-  
-    if (tex.entrytype === 'inproceedings') {
-      tex.remove('pages');
-      tex.remove('volume');
-      tex.remove('address');
-    }
-  
-    if (zotero.itemType === 'webpage' && zotero.url) {
-      tex.add({ name: 'howpublished', bibtex: "{\\url{" + tex.enc_verbatim({value: zotero.url}) + "}}" });
-      // Zotero.debug(tex)
-    }
-  
-    if(tex.has.booktitle && ['NIPS','CVPR','ECCV','AAAI','ICCV','PAMI','BMVC','ICML','SPSYMP','SIGKD','ICLRW','IJCV','IJCNN','ICIP','ICLR', 'WACV'].includes(tex.has.booktitle.value))  {
-      tex.has.booktitle.bibtex = tex.has.booktitle.value
-      // Zotero.debug(tex.has.booktitle)
-    }
-  
-    if(tex.has.journal && tex.has.journal.value.startsWith('arXiv'))  {
-      tex.add({ name: 'journal', value: tex.has.journal.value.replace(/\s*\[.*]\s*/i, '')});
-      // Zotero.debug(tex.has.journal)
-    }
   }
+
+  if (zotero.itemType === 'webpage' && zotero.url) {
+    tex.add({ name: 'howpublished', bibtex: "{\\url{" + tex.enc_verbatim({value: zotero.url}) + "}}" });
+    // Zotero.debug(tex)
+  }
+
+  if(tex.has.booktitle && ['NIPS','CVPR','ECCV','AAAI','ICCV','BMVC','ICML','SPSYMP','SIGKD','ICLRW','IJCNN','ICIP','ICLR', 'WACV'].includes(tex.has.booktitle.value))  {
+    tex.has.booktitle.bibtex = tex.has.booktitle.value
+    // Zotero.debug(tex.has.booktitle)
+  }
+
+  if(tex.has.journal && tex.has.journal.value.startsWith('arXiv'))  {
+    tex.add({ name: 'journal', value: tex.has.journal.value.replace(/\s*\[.*]\s*/i, '')});
+    // Zotero.debug(tex.has.journal)
+  }
+
+  if(tex.has.journal && ['PAMI', 'IJCV','TOG','TIP','TVCG','TCSVT','TMM','ACMMM'].includes(tex.has.journal.value))  {
+    tex.has.journal.bibtex = tex.has.journal.value
+    // Zotero.debug(tex.has.journal.value)
+    // Zotero.debug(tex.has.journal.bibtex)
+  }
+//,
+}
