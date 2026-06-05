10258be9462e9b248b604146b77338600e9992b4
Georg Krispel <georg@krispel.eu>
2026-06-05 23:39:44 +0200

    eza fix

--- ubuntu.sh
+++ ubuntu.sh
@@ -24,7 +24,7 @@ if ! command_exists eza; then
     else
         print_section 2 "Installing eza..."
         curl -Lo /tmp/eza.tar.gz \
-            "https://github.com/eza-community/eza/releases/latest/download/eza_${ARCH}-unknown-linux-musl.tar.gz"
+            "https://github.com/eza-community/eza/releases/latest/download/eza_${ARCH}-unknown-linux-gnu.tar.gz"
         $SUDO tar -xzf /tmp/eza.tar.gz -C /usr/local/bin eza
         rm /tmp/eza.tar.gz
         print_section 2 "✅ eza installed"
